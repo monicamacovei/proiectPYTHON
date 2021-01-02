ae2d2fec6d5472f229caeb66038be9e50b989e27
monicuta2008 <monicuta2008@yahoo.com>
2021-01-02 17:23:13 +0200

    Ajustat cateva warning-uri

--- master.py
+++ master.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def get_country_links():
     response = requests.get("https://www.alexa.com/topsites/countries")  #iau HTML-ul de pe pagina
-    soup = BeautifulSoup(response.text) #initializam BeautifulSoup  (care ne ajuta sa parsam HTML-ul)
+    soup = BeautifulSoup(response.text, features="html.parser") #initializam BeautifulSoup  (care ne ajuta sa parsam HTML-ul)
 
     links = set()  #folosesc set pentru a nu avea duplicate in link-uri
     for link in soup.findAll('a'):  #caut tag-urile HTML a
@@ -17,7 +17,7 @@ def get_country_links():
 
 def get_top_links(country_link):
     response = requests.get(country_link)
-    soup = BeautifulSoup(response.text) 
+    soup = BeautifulSoup(response.text, features="html.parser") 
 
     links = set()  
     for link in soup.findAll('a'):  
@@ -29,4 +29,5 @@ def get_top_links(country_link):
 
 if __name__ == "__main__":
     country_links = get_country_links()
-    print(country_links)
+    top_links = get_top_links("https://www.alexa.com/topsites/countries/ES");
+    print(top_links)
