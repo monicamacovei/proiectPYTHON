79257d9570a41c0b887d324f5968c45c3a3ca535
monicuta2008 <monicuta2008@yahoo.com>
2021-01-02 17:16:32 +0200

    Am terminat get_country_links()

--- master.py
+++ master.py
@@ -5,15 +5,16 @@ from bs4 import BeautifulSoup
 
 def get_country_links():
     response = requests.get("https://www.alexa.com/topsites/countries")  #iau HTML-ul de pe pagina
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text) #initializam BeautifulSoup  (care ne ajuta sa parsam HTML-ul)
 
-    links = set()
-    for link in soup.findAll('a'):
-        link = link.get('href')
+    links = set()  #folosesc set pentru a nu avea duplicate in link-uri
+    for link in soup.findAll('a'):  #caut tag-urile HTML a
+        link = "https://www.alexa.com/topsites/" + link.get('href')
         if "countries/" in link:
             links.add(link)
 
-    print(links)
+    return list(links)
 
 if __name__ == "__main__":
-    get_country_links()
+    country_links = get_country_links()
+    print(country_links)
