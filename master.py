ec1aede1bdb07423b4fee1e43a9582a24e3331d9
monicuta2008 <monicuta2008@yahoo.com>
2021-01-03 14:53:32 +0200

    Adaugat functia alexa_request

--- master.py
+++ master.py
@@ -6,31 +6,31 @@ import sys
 import os 
 from config import locatieDisk
 
-
-def get_country_links():
-    response = requests.get("https://www.alexa.com/topsites/countries")  #iau HTML-ul de pe pagina
+def alexa_request(link, prefix, country_links):
+    response = requests.get(link)  #iau HTML-ul de pe pagina
     soup = BeautifulSoup(response.text, features="html.parser") #initializam BeautifulSoup  (care ne ajuta sa parsam HTML-ul)
 
-    links = set()  #folosesc set pentru a nu avea duplicate in link-uri
+    links = []  
     for link in soup.findAll('a'):  #caut tag-urile HTML a
-        link_text = link.text
-        url = "https://www.alexa.com/topsites/" + link.get('href')
-        if "countries/" in url:
-            links.add((url,link_text))
+        if country_links == True:
+            link_text = link.text
+            link = "https://www.alexa.com/topsites/" + link.get('href')
+        else:
+            link = link.get('href')
+        
+        if prefix in link:
+            if country_links == True:
+                links.append((link,link_text))
+            else:
+                links.append(link.replace("/siteinfo/", ""))
 
-    return list(links)
+    return links
+
+def get_country_links():
+    return alexa_request("https://www.alexa.com/topsites/countries", "countries/", True)
 
 def get_top_links(country_link):
-    response = requests.get(country_link)
-    soup = BeautifulSoup(response.text, features="html.parser") 
-
-    links = set()  
-    for link in soup.findAll('a'):  
-        link = link.get('href')
-        if "siteinfo/" in link:
-            links.add(link.replace("/siteinfo/", ""))
-
-    return list(links)
+    return alexa_request(country_link, "siteinfo/", False)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
