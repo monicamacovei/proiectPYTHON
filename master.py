61c1cb5c88ca598f410baee6f033afbe2ac899ef
monicuta2008 <monicuta2008@yahoo.com>
2021-01-03 15:33:34 +0200

    Adauga comentarii si parametru cu nr procese

--- master.py
+++ master.py
@@ -7,22 +7,22 @@ import os
 from config import locatieDisk
 
 def alexa_request(link, prefix, country_links):
-    response = requests.get(link)  #iau HTML-ul de pe pagina
+    response =  requests.get(link)  #iau HTML-ul de pe pagina
     soup = BeautifulSoup(response.text, features="html.parser") #initializam BeautifulSoup  (care ne ajuta sa parsam HTML-ul)
 
     links = []  
     for link in soup.findAll('a'):  #caut tag-urile HTML a
-        if country_links == True:
-            link_text = link.text
-            link = "https://www.alexa.com/topsites/" + link.get('href')
+        if country_links == True: #daca luam numele si link-ul tarilor
+            link_text = link.text #iau numele tarii din texul tag-ului "a"
+            link = "https://www.alexa.com/topsites/" + link.get('href') #creez link-ul tarii
         else:
-            link = link.get('href')
+            link = link.get('href') #pentru top sites am nevoie doar de link
         
         if prefix in link:
             if country_links == True:
-                links.append((link,link_text))
+                links.append((link,link_text)) #iau link-ul tarii si numele ei
             else:
-                links.append(link.replace("/siteinfo/", ""))
+                links.append(link.replace("/siteinfo/", "")) #doar link-ul site-ului si sterg /siteinfo/ din link
 
     return links
 
@@ -33,20 +33,20 @@ def get_top_links(country_link):
     return alexa_request(country_link, "siteinfo/", False)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if not len(sys.argv) >= 2: #primul argument e obligatoriu (numele cozii)
         print("Trebuie dat ca argument numele cozi din redis")
         exit(0)
-    redis_name = sys.argv[1]
-    q = CustomQueue(Redis(), redis_name)
-    q.empty_queue()
+    redis_name = sys.argv[1] #numele cozii
+    q = CustomQueue(Redis(), redis_name) #initializam coada custom definita in rqueue.py
+    q.empty_queue() #golim coada initializata (in caz ca au ramas date de la ultima rulare)
 
-    country_links = get_country_links()
+    country_links = get_country_links() 
 
     for country_link, country_text in country_links:
-        country_text = country_text.replace(" ", "")
+        country_text = country_text.replace(" ", "") #stergem spatiul din numele tarii
         top_links = get_top_links(country_link)
         for link in top_links:
-            locatieDiskTemp = os.path.join(locatieDisk, country_text)
-            q.enqueue(link,locatieDiskTemp)
-        break
+            locatieDiskTemp = os.path.join(locatieDisk, country_text) #locatia paginii descarcate va fi folderul setat in config si numele tarii
+            q.enqueue(link,locatieDiskTemp) #adauga in coada
+        #break  #ca sa testam pentru o singura tara
 
--- rqueue.py
+++ rqueue.py
@@ -6,20 +6,20 @@ class CustomQueue:
         self.r = r
     
     def enqueue(self, link, locatieDisk):
-        data = json.dumps({"link": link, "locatieDisk": locatieDisk})
+        data = json.dumps({"link": link, "locatieDisk": locatieDisk}) #transform din dictionar in string JSON
         print(data)
-        self.r.lpush(self.name, data)
+        self.r.lpush(self.name, data) #adaugam in redis (la inceputul cozii)
     
     def dequeue(self):
 
-        data = self.r.rpop(self.name)
+        data = self.r.rpop(self.name) #luam din coada self.name ultimul element de la dreapta (adica primul adaugat)
         if not data:
             return None
-        data = data.decode()
-        return json.loads(data)
+        data = data.decode() #transformam din biti in string 
+        return json.loads(data) #transformam string-ul cu JSON in dictionar
 
     def get_len(self):
         return self.r.llen(self.name)
 
     def empty_queue(self):
-        self.r.delete(self.name)
+        self.r.delete(self.name) #golim toata coada self.name
